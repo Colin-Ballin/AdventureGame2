b1cb6a70359fb2848cb1075d45577bbb9ef976fd
robertnovakowski <robertschrbook@gmail.com>
2024-10-04 11:26:41 -0700

    Added card and player files (And .gitignore?)

---
+++ adventureGame2/src/card.java
@@ -0,0 +1,16 @@
+import Stack;
+
+public class card {
+
+    private String element;
+
+    private String name;
+
+    private String type;
+
+    public card(String element, String name, String type){
+        this.element = element;
+        this.name = name;
+        this.type = type;
+    }
+}
